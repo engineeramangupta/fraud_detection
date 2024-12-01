36451f9133c09187ad707caf1f1d08fcb4d873ed
engineeramangupta <amu.aman19@gmail.com>
2024-12-01 16:25:01 +0530

    updated file

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,2 @@
 #  Right now I am learning about Github
 print('Adding all the dataprocessing code - related to fraud detection ! ')
-
-a = 5
-b= 7
-def sum(a,b):
-    return a
-
-result = sum(a,b)
-print(result)
